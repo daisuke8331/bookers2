c77bcc80ed0b89c0b3a4cf804d35871117ea8313
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-09-25 16:59:27 +0000

    bookers2-step3

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
+
+
+
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: "homes#top" #ルートディレクトリ設定
+  get "/homes/about" => "homes#about", as: "about"
 
   resources :users
   resources :books
 
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 #記載があったrouting
